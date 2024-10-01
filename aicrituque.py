1d78a8afadd52ef4328838757f4dcded72eb7863
rexie199x <celeriorex@gmail.com>
2024-10-01 18:55:57 +0800

    Update aicrituque.py

--- aicrituque.py
+++ aicrituque.py
@@ -5,3 +5,27 @@ import os
 # Retrieve API key from environment variables
 api_key = os.getenv("OPENAI_API_KEY")
 client = openai.OpenAI(api_key=api_key)
+
+
+image_question = input("Enter your prompt here and clearly describe what you want the model to tell you about the image: ")
+
+response = client.chat.completions.create(
+  model="gpt-4-vision-preview",
+  messages=[
+    {
+      "role": "assistant",
+      "content": [
+        {"type": "text", "text": image_question},
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": "https://shorturl.at/erAGY", #Mona Lisa painting
+          },
+        },
+      ],
+    }
+  ],
+  max_tokens=300,
+)
+
+print(response.choices[0])                       
